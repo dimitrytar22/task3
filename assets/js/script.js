80072b1530e73072926b35bf5fb06ac7893a4c9f
dimitrytar22 <dtarasenko212006@gmail.com>
2025-02-14 00:36:28 +0200

    added deleting feature with confirmation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 const table = document.getElementById('users');
 const tBody = table.getElementsByTagName('tbody')[0];
 const selectAllCheckbox = document.getElementById('select-all');
+const deleteConfirm = document.getElementById('delete-confirm-modal');
+
+let idsToDelete = [];
 
 selectAllCheckbox.addEventListener('change', function (event) {
     let checked = event.target.checked;
@@ -15,15 +18,54 @@ selectAllCheckbox.addEventListener('change', function (event) {
 
 });
 
-tBody.addEventListener('change', function (event){
+tBody.addEventListener('change', function (event) {
     let emptyCheckboxes = allCheckboxesSelected(tBody);
-    if(emptyCheckboxes)
+    if (emptyCheckboxes)
         selectAllCheckbox.checked = false;
     else
         selectAllCheckbox.checked = true;
 
 });
-const allCheckboxesSelected = function (tBody){
+
+tBody.addEventListener('mouseover', function (event) {
+    let action = event.target.dataset.action;
+    if (action === 'edit') {
+        event.target.style.cursor = 'pointer'
+    } else if (action === 'delete') {
+        event.target.style.cursor = 'pointer'
+    }
+});
+tBody.addEventListener('click', function (event) {
+    let action = event.target.dataset.action;
+    if (action === 'edit') {
+
+    } else if (action === 'delete') {
+        let id = event.target.closest('tr').dataset.id;
+        idsToDelete.push(event.target.closest('tr').dataset.id);
+        deleteConfirm.addEventListener('click', deleteItem);
+    }
+});
+const deleteItem = function (event) {
+    if (event.target.dataset.action === 'confirm') {
+        $.ajax({
+            url: '/handler.php',
+            method: 'get',
+            dataType: 'html',
+            data: {ids: idsToDelete},
+            success: function (data) {
+                console.log(JSON.parse(data));
+            }
+        });
+
+    }
+    idsToDelete = [];
+    deleteConfirm.removeEventListener('click', deleteItem);
+    $(this).modal('hide');
+}
+
+
+
+const allCheckboxesSelected = function (tBody) {
     let checkboxCount = tBody.querySelectorAll('input').length;
     let selectedCheckboxCount = tBody.querySelectorAll('input:checked').length;
     return checkboxCount !== selectedCheckboxCount;
--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <title>Title</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
           integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js"></script>
 </head>
 <body>
@@ -36,7 +37,7 @@
                         <div class="mb-3">
                             <label for="role" class="col-form-label">Role:</label>
                             <select class="w-50 form-select" aria-label="Default select example">
-                                <option selected>Please select</option>
+                                <option selected id="role">Please select</option>
                                 <option value="1">Admin</option>
                                 <option value="2">User</option>
                             </select>
@@ -53,6 +54,24 @@
     <div class="d-grid gap-2 d-md-block">
         <button class="btn btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#user-modal">Add</button>
     </div>
+
+
+    <div class="modal fade" id="delete-confirm-modal" tabindex="-1" aria-labelledby="delete-confirm-modal"
+         aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="delete-confirm-modal-label">Are you sure?</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" data-action="cancel">Cancel</button>
+                    <button type="button" class="btn btn-primary btn-danger" data-action="confirm">Delete</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <table class="table" id="users">
         <thead>
         <tr>
@@ -68,7 +87,7 @@
         </tr>
         </thead>
         <tbody>
-        <tr>
+        <tr  data-id="1">
 
             <th scope="row">
                 <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
@@ -83,11 +102,13 @@
                 </svg>
             </td>
             <td>User</td>
-            <td><img src="assets/images/edit.png" class="img-fluid cursor-pointer" data-action="edit" alt="edit">
-                <img src="assets/images/delete.png" class="img-fluid cursor-pointer" data-action="delete" alt="delete">
+            <td id="actions"><img src="assets/images/edit.png" class="img-fluid cursor-pointer" data-bs-toggle="modal"
+                     data-bs-target="#user-modal" data-action="edit"  alt="edit">
+                <img src="assets/images/delete.png" class="img-fluid cursor-pointer" data-bs-toggle="modal"
+                     data-bs-target="#delete-confirm-modal" data-action="delete" alt="delete">
             </td>
         </tr>
-        <tr>
+        <tr  data-id="2">
             <th scope="row"><input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
 
             </th>
@@ -100,11 +121,13 @@
                 </svg>
             </td>
             <td>Admin</td>
-            <td><img src="assets/images/edit.png" class="img-fluid cursor-pointer" data-action="edit" alt="edit">
-                <img src="assets/images/delete.png" class="img-fluid cursor-pointer" data-action="delete" alt="delete">
+            <td><img src="assets/images/edit.png" class="img-fluid cursor-pointer" data-bs-toggle="modal"
+                     data-bs-target="#user-modal" data-id="1" data-action="edit" alt="edit">
+                <img src="assets/images/delete.png" class="img-fluid cursor-pointer" data-bs-toggle="modal"
+                     data-bs-target="#delete-confirm-modal" data-id="1" data-action="delete" alt="delete">
             </td>
         </tr>
-        <tr>
+        <tr  data-id="3">
             <th scope="row"><input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
 
             </th>
@@ -118,8 +141,10 @@
             </td>
             <td>Admin</td>
 
-            <td><img src="assets/images/edit.png" class="img-fluid cursor-pointer" data-action="edit" alt="edit">
-                <img src="assets/images/delete.png" class="img-fluid cursor-pointer" data-action="delete" alt="delete">
+            <td ><img src="assets/images/edit.png" class="img-fluid cursor-pointer" data-bs-toggle="modal"
+                     data-bs-target="#user-modal" data-id="1" data-action="edit" alt="edit">
+                <img src="assets/images/delete.png" class="img-fluid cursor-pointer" data-bs-toggle="modal"
+                     data-bs-target="#delete-confirm-modal" data-id="1" data-action="delete" alt="delete">
             </td>
         </tr>
         </tbody>
